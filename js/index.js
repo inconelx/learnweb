e231d76b7077a5c4f644dcda5553d1e159f646f6
inconelx <104505239+inconelx@users.noreply.github.com>
2022-11-28 21:47:37 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 function index_select(){
+    document.cookie = "select=1";
     let obj = document.getElementsByClassName("elems");
     for(let i = 0 ; i < obj.length; ++i){
         obj[i].onclick = () =>{
-            let dc = "select=" + i;
+            let dc = "select=" + (i+1);
             document.cookie = dc;
             window.location.replace('detail.html');
         }
