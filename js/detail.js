d830c21c4f42b8f76fc1a01e6cd86e01803e93e6
inconelx <104505239+inconelx@users.noreply.github.com>
2022-11-28 19:56:11 +0800

    Update detail.js

--- js/detail.js
+++ js/detail.js
@@ -1,4 +1,4 @@
-var post = { imgs:['image/1_0.png','image/1_1.png'],
+var post = { imgs:['image/1_0.png','image/1_1.png','image/1_2.png'],
 dcs:['active','',''],
 info_0:'H',info_1:'氢',info_2:'1.008',
 info_3:'-259℃',info_4:'-253℃',info_5:'非金属',info_6:'1',
